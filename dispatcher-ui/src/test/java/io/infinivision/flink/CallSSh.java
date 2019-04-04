2311ee3fb8edee0eb424a947e109ce87c40cdc52
zhangfeng <smilebackxiaofeng@gmail.com>
2019-04-04 15:34:54 +0800

    create by zhangfeng

--- dispatcher-ui/src/test/java/io/infinivision/flink/CallSSh.java
+++ dispatcher-ui/src/test/java/io/infinivision/flink/CallSSh.java
@@ -22,30 +22,15 @@ public class CallSSh {
             sb.append("/bigdata/flink-1.5.1/lib/log4j-1.2.17.jar:");
             sb.append("/bigdata/flink-1.5.1/lib/slf4j-log4j12-1.7.7.jar:");
             sb.append("/bigdata/flink-1.5.1/lib/flink-dist_2.11-1.5.1.jar:");
-
-            sb.append("/bigdata/hadoop-2.7.5/etc/hadoop:");
-//            sb.append("/bigdata/flink-1.5.1/opt/connectors/kafka010/flink-connector-kafka-0.10_2.11-1.5.1-sql-jar.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/connectors/kafka011/flink-connector-kafka-0.11_2.11-1.5.1-sql-jar.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/connectors/kafka08/flink-connector-kafka-0.8_2.11-1.5.1.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/connectors/kafka09/flink-connector-kafka-0.9_2.11-1.5.1-sql-jar.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/connectors/flink-connector-hadoop-compatibility_2.11-1.5.1.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/connectors/flink-connector-hive_2.11-1.5.1.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/connectors/flink-hbase_2.11-1.5.1.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/sql-client/datanucleus-api-jdo-4.2.4.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/sql-client/datanucleus-core-4.1.17.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/sql-client/datanucleus-rdbms-4.1.19.jar:");
             sb.append("/bigdata/flink-1.5.1/opt/sql-client/flink-sql-client-1.5.1.jar:");
-//            sb.append("/bigdata/flink-1.5.1/opt/sql-client/javax.jdo-3.2.0-m3.jar:");
+            sb.append("/bigdata/hadoop-2.7.5/etc/hadoop:");
 
-            sb.append("/opt/apps/zf/blink-sql/client-jar-1.0-SNAPSHOT.jar com.zf.client.SubmitJarUtil ");
+            sb.append("/opt/apps/zf/blink-sql/dispatcher-1.0.jar io.infinivision.flink.core.Dispatcher ");
             sb.append("-s test ");
-            sb.append("-u /opt/apps/zf/blink-sql/test_join_pg_s.sql ");
             sb.append("-d /bigdata/flink-1.5.1/conf/sql-client-defaults.yaml ");
-            sb.append("-e /bigdata/flink-1.5.1/conf/sql-client-defaults.yaml ");
-            //sb.append("-l /bigdata/flink-1.5.1/lib ");
-            //sb.append("-l /bigdata/flink-1.5.1/opt ");
+            sb.append("-l /bigdata/flink-1.5.1/opt ");
             sb.append("-l /bigdata/flink-1.5.1/opt/connectors ");
-//            sb.append("-l /bigdata/flink-1.5.1/opt/sql-client ");
+            sb.append("-sqlPath /opt/apps/zf/blink-sql/test_join_pg_s.sql ");
 
             Session shellSession = SSHUtils.getJSchSession(host);
 //            stdout = SSHUtils.execCommandByJSch(shellSession, "cd /opt/apps/zf/blink-sql");
