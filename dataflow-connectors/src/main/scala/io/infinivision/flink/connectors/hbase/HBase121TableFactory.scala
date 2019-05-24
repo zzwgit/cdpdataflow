0f41610749d94b655322dbf5878fd8395a8fb672
Hongtao Zhang <553780043@qq.com>
2019-05-24 14:04:44 +0800

    initial commit for hbase connector

---
+++ dataflow-connectors/src/main/scala/io/infinivision/flink/connectors/hbase/HBase121TableFactory.scala
@@ -0,0 +1,7 @@
+package io.infinivision.flink.connectors.hbase
+
+import org.apache.flink.connectors.hbase.table.HBaseTableFactoryBase
+
+class HBase121TableFactory extends HBaseTableFactoryBase{
+
+}
