72758d0e10e9d800e34f915367ea22db15c0ae47
pengmingguo <pengmingguo@hotmail.com>
2019-08-06 16:46:15 +0800

    split connector module into postgres hbase jdbc clickhouse etc. module
    fix NullPointerException by zhangfeng

---
+++ dataflow-connectors/dataflow-connector-jdbc/src/main/java/io/infinivision/flink/connectors/http/HttpTableFactory.java
@@ -0,0 +1,109 @@
+package io.infinivision.flink.connectors.http;
+
+import io.infinivision.flink.connectors.utils.JDBCTableOptions;
+import org.apache.flink.api.java.io.jdbc.JDBCOptions;
+import org.apache.flink.table.api.RichTableSchema;
+import org.apache.flink.table.api.types.InternalType;
+import org.apache.flink.table.dataformat.BaseRow;
+import org.apache.flink.table.factories.BatchTableSourceFactory;
+import org.apache.flink.table.factories.StreamTableSourceFactory;
+import org.apache.flink.table.sources.BatchTableSource;
+import org.apache.flink.table.sources.StreamTableSource;
+import org.apache.flink.table.util.TableProperties;
+import org.apache.flink.util.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+import static org.apache.flink.table.descriptors.ConnectorDescriptorValidator.CONNECTOR_PROPERTY_VERSION;
+import static org.apache.flink.table.descriptors.ConnectorDescriptorValidator.CONNECTOR_TYPE;
+
+public class HttpTableFactory implements
+        StreamTableSourceFactory<BaseRow>,
+        BatchTableSourceFactory<BaseRow> {
+    private static final Logger LOG = LoggerFactory.getLogger(HttpTableFactory.class);
+
+    @Override
+    public StreamTableSource<BaseRow> createStreamTableSource(Map<String, String> properties) {
+        return createTableSource(properties);
+    }
+
+    @Override
+    public BatchTableSource<BaseRow> createBatchTableSource(Map<String, String> properties) {
+        return  createTableSource(properties);
+    }
+
+    @Override
+    public Map<String, String> requiredContext() {
+        Map<String, String> context = new HashMap<>();
+        context.put(CONNECTOR_TYPE, "HTTP");
+        context.put(CONNECTOR_PROPERTY_VERSION, "1");
+        return context;
+    }
+
+    @Override
+    public List<String> supportedProperties() {
+        List<String> properties = new ArrayList<>(JDBCOptions.SUPPORTED_KEYS);
+        properties.addAll(JDBCTableOptions.SUPPORTED_KEYS);
+        properties.addAll(HttpTableOptions.SUPPORTED_KEYS);
+        return properties;
+    }
+
+    private HttpTableSource createTableSource(Map<String, String> properties) {
+        TableProperties prop = new TableProperties();
+        prop.putProperties(properties);
+
+        RichTableSchema schema = prop.readSchemaFromProperties(
+                Thread.currentThread().getContextClassLoader()
+        );
+        String[] columnNames = schema.getColumnNames();
+        InternalType[] columnTypes = schema.getColumnTypes();
+        boolean[] nullables = schema.getNullables();
+
+        Preconditions.checkArgument(columnNames.length > 0 && columnTypes.length > 0 && nullables.length > 0,
+                "column numbers can not be 0");
+
+        Preconditions.checkArgument(columnNames.length == columnTypes.length,
+                "columnNames length must be equal to columnTypes length");
+
+        Preconditions.checkArgument(columnNames.length == nullables.length,
+                "columnNames length must be equal to nullable length");
+
+
+        HttpTableSource.Builder builder = HttpTableSource.builder()
+                .fields(columnNames, columnTypes, nullables);
+
+        Set<String> primaryKeys = new HashSet<>();
+        Set<Set<String>> uniqueKeys = new HashSet<>();
+        Set<Set<String>> normalIndexes = new HashSet<>();
+        if (!schema.getPrimaryKeys().isEmpty()) {
+            uniqueKeys.add(new HashSet<>(schema.getPrimaryKeys()));
+            primaryKeys = new HashSet<>(schema.getPrimaryKeys());
+        }
+        for (List<String> uniqueKey : schema.getUniqueKeys()) {
+            uniqueKeys.add(new HashSet<>(uniqueKey));
+        }
+        for (RichTableSchema.Index index : schema.getIndexes()) {
+            if (index.unique) {
+                uniqueKeys.add(new HashSet<>(index.keyList));
+            } else {
+                normalIndexes.add(new HashSet<>(index.keyList));
+            }
+        }
+        if (!primaryKeys.isEmpty()) {
+            builder.setPrimaryKeys(primaryKeys);
+        }
+        if (!uniqueKeys.isEmpty()) {
+            builder.setUniqueKeys(uniqueKeys);
+        }
+        if (!normalIndexes.isEmpty()) {
+            builder.setNormalIndexes(normalIndexes);
+        }
+
+        builder.setTableProperties(prop);
+
+        return builder.build();
+    }
+
+}
