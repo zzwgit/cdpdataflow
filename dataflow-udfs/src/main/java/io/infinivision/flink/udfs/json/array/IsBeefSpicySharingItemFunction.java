ef45db626fb2fc23645e48b42c555601a19b653e
pengmingguo <pengmingguo@hotmail.com>
2019-12-27 08:39:40 +0800

    更新udf逻辑

--- dataflow-udfs/src/main/java/io/infinivision/flink/udfs/json/array/IsBeefSpicySharingItemFunction.java
+++ dataflow-udfs/src/main/java/io/infinivision/flink/udfs/json/array/IsBeefSpicySharingItemFunction.java
@@ -13,11 +13,7 @@ public class IsBeefSpicySharingItemFunction extends ScalarFunction {
 		String type = (String) args[0];
 		LinkedList<Integer> result = new LinkedList<>();
 		JSONArray level1;
-		if (args[1] instanceof JSONArray) {
-			level1 = (JSONArray) args[1];
-		} else {
-			level1 = JSON.parseArray((String) args[1]);
-		}
+		level1 = Utils.getOrParse(args[1]);
 
 		if (beefSpicy.contains(type)) {
 			JSONArray protein = Utils.getOrParse(args[2]);
@@ -33,24 +29,22 @@ public class IsBeefSpicySharingItemFunction extends ScalarFunction {
 				}
 				String var2 = protein.getString(i);
 				if (type.contains("beef")) {
-					result.add(var2.contains("BEEF") ? 1 : 0);
+					result.add(var2.toLowerCase().contains("beef") ? 1 : 0);
 				} else {
 					String var3 = level8.getString(i);
+					boolean chicken = var2.toLowerCase().contains("chicken");
+					boolean spicy = var3.toLowerCase().contains("spicy");
 					if (type.contains("non")) {
-						result.add(var2.contains("CHICKEN") && !var3.contains("Spicy") ? 1 : 0);
+						result.add(chicken && !spicy ? 1 : 0);
 					} else {
-						result.add(var2.contains("CHICKEN") && var3.contains("Spicy") ? 1 : 0);
+						result.add(chicken && spicy ? 1 : 0);
 					}
 				}
 			}
 		} else {
 			JSONArray level3 = Utils.getOrParse(args[2]);
 			JSONArray qty;
-			if (args[3] instanceof JSONArray) {
-				qty = (JSONArray) args[3];
-			} else {
-				qty = JSON.parseArray((String) args[3]);
-			}
+			qty = Utils.getOrParse(args[3]);
 			if (level1.size() < 5) {
 				for (int i = 0; i < level1.size(); i++) {
 					result.add(0);
@@ -60,7 +54,7 @@ public class IsBeefSpicySharingItemFunction extends ScalarFunction {
 				for (int i = 0; i < level1.size(); i++) {
 					String var2 = level3.getString(i);
 					Integer var3 = qty.getInteger(i);
-					if (var2.contains("Burger") && var3 != null) {
+					if (var2.toLowerCase().contains("burger") && var3 != null) {
 						burgerCnt += var3;
 					}
 				}
@@ -68,7 +62,7 @@ public class IsBeefSpicySharingItemFunction extends ScalarFunction {
 					String var1 = level1.getString(i);
 					String var2 = level3.getString(i);
 					Integer var3 = qty.getInteger(i);
-					if (var1.contains("Happy Meal") || var2.contains("Burger") && burgerCnt > 1) {
+					if (var1.contains("Happy Meal") || var2.toLowerCase().contains("burger") && burgerCnt > 1) {
 						result.add(1);
 					} else {
 						result.add(0);
